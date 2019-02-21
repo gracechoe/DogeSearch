74e12dfda49060dced0889d40ee0cd6b24a050e9
Jenny Chau <jchau9@uci.edu>
2019-02-21 15:55:19 -0800

    able to parse file

--- main.py
+++ main.py
@@ -6,17 +6,28 @@
 #corresponding postings list include: document name/id token was found in, word frequency, 
 #indices of occurence within the document, tf-idf score
 #store this shit in a database (probably MongoDB)
-import os
 import json
+from bs4 import BeautifulSoup
 
 def access_files():
     path = "/Users/macbookpro/Documents/WEBPAGES_RAW/"
     bookkeeping = open(path+"bookkeeping.json", "r")
     data = json.load(bookkeeping)
     for key in data:
-        f = open(path+key)
-        print(f.read())
-        
+        parse_file(path+key)
+
+def parse_file(path):
+    f = open(path)
+    soup = BeautifulSoup(f.read(), "html.parser")
+    soup.prettify()
+    #print(soup.get_text())
+    print(find_tags(soup))
+
+
+def find_tags(soup):
+    result = soup.findAll(["body", "title", "h1", "h2", "h3", "b", "strong"])
+    #return result
+    return soup
 
 if __name__ == "__main__":
     access_files()
