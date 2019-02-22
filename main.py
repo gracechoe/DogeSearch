e340c74dfdf4b74de4c68e749fbdf1af7ce90b81
gracechoe <gracechoe925@gmail.com>
2019-02-22 02:03:17 -0800

    everything

--- main.py
+++ main.py
@@ -7,20 +7,48 @@
 #indices of occurence within the document, tf-idf score
 #store this shit in a database (probably MongoDB)
 import json
-from bs4 import BeautifulSoup
-import nltk
 import re
+import math
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
 def access_files():
-    path = "/Users/macbookpro/Documents/WEBPAGES_RAW/"
+    path = "/Users/gracechoe/Documents/WEBPAGES_RAW/"
     bookkeeping = open(path+"bookkeeping.json", "r")
     data = json.load(bookkeeping)
-    #for key in data:
-    parse_file(path+"0/33")
+    count = 0
+    for key in data:
+        if count > 20:
+            break
+        process_file(path,key)
+        count += 1
+    complete_index()
 
-def parse_file(path):
+def complete_index():
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["INVERTED_INDEX"]
+    col = db["index"]
+    col2 = db["docs"]
+    doc_count = col2.count()
+
+    for entry in col.find():
+        docs = entry['documents']
+        freqs = entry['word_freq']
+        token = entry['token']
+        for doc, freq in zip(docs, freqs):
+            result = col2.find({'doc': doc})
+            result = result.next()
+            term_count = result['terms_count']
+            tf = float(freq) / term_count
+            idf = math.log(float(doc_count)/len(docs))
+            tf_idf = tf*idf
+            print(tf, idf, tf_idf)
+            col.update({'token':token},{"$push":{'tf-idf':tf_idf}})
+
+
+def process_file(path, key):
     freq_dict = {}
-    f = open(path)
+    f = open(path+key)
     soup = BeautifulSoup(f.read(), "html.parser")
     soup.prettify()
     text = find_tags(soup).encode('utf-8')
@@ -32,10 +60,31 @@ def parse_file(path):
             freq_dict[token] = 1
     print(freq_dict)
 
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["INVERTED_INDEX"]
+    col = db["index"]
+    col2 = db["docs"]
+
+    for token, freq in freq_dict.items():
+        if col.find({'token': token}).count() > 0:
+            query = {'token': token}
+            new_values = {"$push": {'documents': key, 'word_freq': freq}}
+            col.update_one(query, new_values)
+        else:
+            entry = {
+                'token': token, 'documents': [key], 'word_freq': [freq], 'tf-idf': []
+            }
+            result = col.insert(entry)
+
+    entry = {'doc':key, 'terms_count':len(tokens)}
+    col2.insert(entry)
+
+
 def find_tags(soup):
     [s.extract() for s in soup(['style', 'script', 'head'])]
     result = soup.get_text()
     return result
+
 
 if __name__ == "__main__":
     access_files()
