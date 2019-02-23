7de10f4eae489818bbf9145b52e5c6abbd94af19
Jenny Chau <jchau9@uci.edu>
2019-02-22 19:35:38 -0800

    code cleanup and added some analytic requirements

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-#go through bookkeeping json in loop
-#access file through given path in bookkeeping key
-#use beautiful soup to parse through content of file
-#extract tokens from file content
-#add to list of corresponding postings for token key
-#corresponding postings list include: document name/id token was found in, word frequency, 
-#indices of occurence within the document, tf-idf score
-#store this shit in a database (probably MongoDB)
 import json
 import re
 import math
@@ -18,9 +10,8 @@ def access_files():
     data = json.load(bookkeeping)
     count = 0
     for key in data:
-        if count > 10:
-           break
-        process_file(path,key)
+        if count < 1000:
+            process_file(path,key)
         count += 1
         print(count)
     complete_index()
@@ -44,7 +35,6 @@ def complete_index():
             tf = float(freq) / term_count
             idf = math.log(float(doc_count)/len(docs))
             tf_idf = tf*idf
-            #print(doc)
             col.update({'token':token},{"$push":{'tf-idf':tf_idf}})
 
 
@@ -60,26 +50,25 @@ def process_file(path, key):
             freq_dict[token] += 1
         else:
             freq_dict[token] = 1
-    #print(freq_dict)
 
     client = MongoClient("mongodb://localhost:27017/")
     db = client["INVERTED_INDEX"]
-    col = db["index"]
-    col2 = db["docs"]
+    index_col = db["index"]
+    doc_col = db["docs"]
 
     for token, freq in freq_dict.items():
-        if col.find_one({'token': token}):
+        if index_col.find_one({'token': token}):
             query = {'token': token}
             new_values = {"$push": {'documents': key, 'word_freq': freq}}
-            col.update_one(query, new_values)
+            index_col.update_one(query, new_values)
         else:
             entry = {
                 'token': token, 'documents': [key], 'word_freq': [freq], 'tf-idf': []
             }
-            result = col.insert(entry)
+            result = index_col.insert(entry)
 
     entry = {'doc':key, 'terms_count':len(tokens)}
-    col2.insert(entry)
+    doc_col.insert(entry)
 
 
 def find_tags(soup):
@@ -90,15 +79,12 @@ def find_tags(soup):
 def create_output_file():
     client = MongoClient("mongodb://localhost:27017/")
     db = client["INVERTED_INDEX"]
-    col = db["index"]
-    col2 = db["docs"]
     output_str = "PROGRAM ANALYTICS"
     output_str += "\n Number of Documents: "
-    output_str += str(col2.find().count())
+    output_str += str(db["docs"].find().count())
     output_str += "\n Number of Unique Words: "
-    output_str += str(col.find().count())
+    output_str += str(db["index"].find().count())
     output_str += "\n Total size of Index on Disk (in KB):"
-    #output_str += str(col.stats(1024)["totalIndexSize"])
     output_str += str(db.command({"collStats":"index", "scale":1024})["totalIndexSize"])
     output_str += "\n URL results for Query [Informatics]: \n"
     output_str += get_urls("Informatics")
@@ -113,11 +99,10 @@ def create_output_file():
 def get_urls(token):
     client = MongoClient("mongodb://localhost:27017/")
     db = client["INVERTED_INDEX"]
-    col = db["index"]
     result = ""
     bookkeeping = open("/Users/macbookpro/Documents/WEBPAGES_RAW/bookkeeping.json", "r")
     data = json.load(bookkeeping)
-    entry = col.find_one({'token':token.lower()})
+    entry = db["index"].find_one({'token':token.lower()})
     if entry:
         documents = entry["documents"]
         count = 0
@@ -125,6 +110,7 @@ def get_urls(token):
             if count < 20:
                 result += data[doc] + "\n"
                 count += 1
+        result += entry["documents"].count()
     return result
 
 if __name__ == "__main__":
