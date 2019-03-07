e122f1723b5d97b3d3f728e5157cfcfc9b496fcc
Jenny Chau <jchau9@uci.edu>
2019-03-07 03:04:09 -0800

    take in more than one token query

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import math
 import sys
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+from collections import defaultdict
+from nltk.stem.snowball import SnowballStemmer
 
 """
 Notes: 
@@ -21,6 +23,7 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["INVERTED_INDEX"]
 index_col = db["index"]
 doc_col = db["docs"]
+stemmer = SnowballStemmer("english")
 
 # initialize() goes through each key in bookkeeping.json and calls process_file on it.
 def initialize():
@@ -44,6 +47,7 @@ def process_file(key):
     global path
     global index_col
     global doc_col
+    global stemmer
 
     # read the file
     f = open(path+key)
@@ -60,6 +64,7 @@ def process_file(key):
         freq_dict = {}
 
         for token in tokens:
+            token = stemmer.stem(token)
             if token_count > 10000:
                 raise Exception('Token dict too long')
             if len(token) < 20:
@@ -114,14 +119,16 @@ def complete_index():
 
         for doc, freq in zip(docs, freqs):
             result = doc_col.find_one({'doc': doc})
-            #result = result.next() #???
-            term_count = result['terms_count']
+            try: 
+                term_count = result['terms_count']
+                tf = float(freq) / term_count 
+                idf = math.log(float(doc_count)/len(docs))
+                tf_idf = tf*idf
 
-            tf = float(freq) / term_count 
-            idf = math.log(float(doc_count)/len(docs))
-            tf_idf = tf*idf
-
-            index_col.update({'token':token},{"$push":{'tf-idf':tf_idf}})
+                index_col.update({'token':token},{"$push":{'tf-idf':tf_idf}})
+            except:
+                print("caught an error")
+                pass
 
 # creates output file for the program analytics with various kinds of info
 def create_output_file():
@@ -152,22 +159,33 @@ def create_output_file():
 # retrieves all urls that contain the token, returns a string with 20 of those urls and total url count
 def get_urls(token):
     global data
-    global index_col
     result = ""
-    
-    entry = index_col.find_one({'token':token.lower()})
     count = 0
-    if entry:
-        ranked = sorted(zip(entry["tf-idf"], entry["documents"]), key=lambda pair: -pair[0])
-        print(ranked)
-        for _, doc in ranked:
-            if count < 20:
-                result += data[doc] + "\n"
-            count += 1
-            
-    result += "total count: " + str(count)
+
+    query_dict = compute_queries(token.split(" "))
+    ranked = sorted(query_dict.items(), key=lambda k: (-k[1][1], -k[1][0]))
+    
+    for doc, _ in ranked:
+        if count < 20:
+            result += data[doc] + "\n"
+        count += 1
     print(result)
     return result
+
+def compute_queries(tokens):
+    global index_col
+    global stemmer
+    d = defaultdict(list)
+    for token in tokens:
+        entry = index_col.find_one({'token':stemmer.stem(token).lower()})
+        if entry:
+            for doc, tf_idf in zip(entry["documents"], entry["tf-idf"]):
+                if doc in d:
+                    d[doc][0] += tf_idf
+                    d[doc][1] += 1
+                else:
+                    d[doc] = [tf_idf, 1]
+    return d
 
 if __name__ == "__main__":
     initialize()
