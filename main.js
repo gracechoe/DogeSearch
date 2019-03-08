66a807b0fa6be70007433136ad7423410a759453
Jenny Chau <jchau9@uci.edu>
2019-03-07 17:46:48 -0800

    update html and js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function getQueryResults(){
         url: "http://127.0.0.1:5000/get_urls/" + query,
         success: function(result){
             $("#result_label").empty()
-            let placeholder = result.length == 0 ? "much empty" : "wow meny results"
+            let placeholder = result.length == 0 ? "much empty" : "wow so meny results"
             $("#result_label").append(placeholder)
             for (let i = 0; i < result.length; i++){
                 let count = i+1;
