ebcb50807aa135eb4eca71c4abfb7744f65addd4
Jenny Chau <jchau9@uci.edu>
2019-03-07 17:34:14 -0800

    add smoll customizations

--- main.js
+++ main.js
@@ -5,13 +5,12 @@ function getQueryResults(){
         url: "http://127.0.0.1:5000/get_urls/" + query,
         success: function(result){
             $("#result_label").empty()
-            if (result.length == 0){
-                $("#result_label").append("much empty")
-            }
+            let placeholder = result.length == 0 ? "much empty" : "wow meny results"
+            $("#result_label").append(placeholder)
             for (let i = 0; i < result.length; i++){
                 let count = i+1;
                 console.log(result[i])
-                $("#result_label").append(count+": <a href=http://" + result[i] + ">" + result[i] + "</a><br/>");
+                $("#result_label").append("<br/>"+count+": <a href=http://" + result[i] + " target=_blank>" + result[i] + "</a>");
             }
         }
     })
